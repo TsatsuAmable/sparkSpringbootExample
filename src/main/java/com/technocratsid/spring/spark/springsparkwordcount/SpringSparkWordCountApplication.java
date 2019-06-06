e35438af79687587fa3f73d10e9df87044360189
Tsatsu Kofi Amable <tsatsu.amable@gmail.com>
2019-06-06 15:34:09 +0000

    Initial commit for Apache Spark and Springboot Integration

---
+++ src/main/java/com/technocratsid/spring/spark/springsparkwordcount/SpringSparkWordCountApplication.java
@@ -0,0 +1,13 @@
+package com.technocratsid.spring.spark.springsparkwordcount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSparkWordCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSparkWordCountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technocratsid/spring/spark/springsparkwordcount/SpringSparkWordCountApplicationTests.java
@@ -0,0 +1,16 @@
+package com.technocratsid.spring.spark.springsparkwordcount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSparkWordCountApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
